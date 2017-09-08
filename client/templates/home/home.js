ec4e98b54235f583a48f9ee9630452364dbc5065
robinBanquo <caramaschi@hotmail.com>
2017-09-08 09:54:25 +0200

    first commit

---
+++ client/templates/home/home.js
@@ -0,0 +1,33 @@
+Template.home.helpers({
+    //add you helpers here
+    categories: [
+        {name: "Pizzas",
+            src:""},
+        {name: "Burgers",
+            src:""},
+        {name: "Salades",
+            src:""},
+        {name: "Boissons",
+            src:""},
+        {name: "Desserts",
+            src:""},
+
+    ]
+});
+
+Template.home.events({
+    //add your events here
+});
+
+Template.home.onCreated(function () {
+    //add your statement here
+});
+
+Template.home.onRendered(function () {
+    //add your statement here
+});
+
+Template.home.onDestroyed(function () {
+    //add your statement here
+});
+
