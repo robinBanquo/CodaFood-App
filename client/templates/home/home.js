75b15dfa3a9f58c73e1642b585cfb07e0119b9c8
robinBanquo <caramaschi@hotmail.com>
2017-09-11 16:48:02 +0200

    plus qu'a styler les prods

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -65,42 +65,8 @@ Template.home.onCreated(function () {
 
 Template.home.onRendered(function () {
     //add your statement here
-    // Instantiate the Bootstrap carousel
-    $('.multi-item-carousel').carousel({
-        interval: false
-    });
-
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-    $('.multi-item-carousel .item').each(function(){
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-
-        if (next.next().length>0) {
-            next.next().children(':first-child').clone().appendTo($(this));
-        } else {
-            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-        }
-    });
 });
 
 Template.home.onDestroyed(function () {
     //add your statement here
 });
-
-/*function slider(centralItem) {
-    console.log(centralItem);
-    let itemsToShow = [
-        centralItem === 0 ? 5: centralItem - 1,
-        centralItem,
-        centralItem === 5 ? 0: centralItem + 1
-    ];
-
-    $(".caroussel-item").addClass("hidden");
-    _.each(itemsToShow, function (itemToshow) {
-        $("#caroussel-"+itemToshow).removeClass("hidden");
-    })
-}*/
